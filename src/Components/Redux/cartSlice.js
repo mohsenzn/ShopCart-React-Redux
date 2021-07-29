e79131bf52b77342faf5520dcdcff3cf0c26a7fe
mohsen.zergani <mohsen.zergani@gmail.com>
2021-07-29 21:13:12 +0430

    install redux

---
+++ src/Components/Redux/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        showCart: false,
+        cartItems: [],
+    },
+    reducers: {
+        
+    }
+})
